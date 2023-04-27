d0912c6d282b5a6e15051f35b2fb0ec8b30037ff
Deepak <deepakkr9098@gmail.com>
2023-04-27 22:19:21 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -50,3 +50,13 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
     except Exception as e:
         logging.info("error occured at evaluate model")
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+            
+    except Exception as e:
+        logging.info("Error occured at load Object function in utils")
+        raise CustomException( e, sys)
