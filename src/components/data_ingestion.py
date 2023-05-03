7c91c482ee7a19cc60c9dd754d7f687a2b8d62cb
Deepak <deepakkr9098@gmail.com>
2023-05-03 11:14:37 +0530

    completed first ml project

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,53 +5,45 @@ from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
 from src.components.data_transformation import DataTransformation
 
 
-
-#Initialize the Data Ingestion Configuration
+## Intitialize the Data Ingetion Configuration
 
 @dataclass
-class DataIngestionConfig:
-    train_data_path:str= os.path.join("artifacts", "train.csv" )
-    test_data_path:str= os.path.join("artifacts", "test.csv" )
-    raw_data_path:str= os.path.join("artifacts", "raw.csv" )
+class DataIngestionconfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv')
 
-##Create a class for Data Ingestion
+## create a class for Data Ingestion
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config= DataIngestionConfig()
+        self.ingestion_config=DataIngestionconfig()
 
     def initiate_data_ingestion(self):
-        logging.info("data ingestion method starts")
+        logging.info('Data Ingestion methods Starts')
         try:
-            df= pd.read_csv(os.path.join("src/notebook/data", "gemstone.csv"))
-            logging.info("dataset read as pandas dataframe")
+            df=pd.read_csv(os.path.join('src/notebook/data','gemstone.csv'))
+            logging.info('Dataset read as pandas Dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info('Train test split')
+            train_set,test_set=train_test_split(df,test_size=0.30,random_state=42)
 
-            logging.info("Train Test Split")
-            train_set, test_set= train_test_split(df, test_size=0.3, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header= True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header= True)
-
-            logging.info("Ingestion of data is completed")
+            logging.info('Ingestion of Data is completed')
 
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-
             )
-
-
-
-        
+  
+            
         except Exception as e:
-            logging.info("Error occured at Data Ingestion Stage")
-            raise CustomException(e, sys)
-
-
-
-
+            logging.info('Exception occured at Data Ingestion stage')
+            raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -1,31 +1,25 @@
-import sys
 import os
-
+import sys
 import pickle
-
+import numpy as np 
 import pandas as pd
-import numpy as np
-
-from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
 from src.exception import CustomException
 from src.logger import logging
 
-
-def save_obj(file_path, obj):
+def save_object(file_path, obj):
     try:
-        dir_path= os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok= True)
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
 
-        
-    
     except Exception as e:
         raise CustomException(e, sys)
-
-
+    
 def evaluate_model(X_train,y_train,X_test,y_test,models):
     try:
         report = {}
@@ -46,17 +40,15 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
             report[list(models.keys())[i]] =  test_model_score
 
         return report
-    
+
     except Exception as e:
-        logging.info("error occured at evaluate model")
-        raise CustomException(e, sys)
-
-
+        logging.info('Exception occured during model training')
+        raise CustomException(e,sys)
+    
 def load_object(file_path):
     try:
-        with open(file_path, "rb") as file_obj:
+        with open(file_path,'rb') as file_obj:
             return pickle.load(file_obj)
-            
     except Exception as e:
-        logging.info("Error occured at load Object function in utils")
-        raise CustomException( e, sys)
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
