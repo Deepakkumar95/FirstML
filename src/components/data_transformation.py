534eaab10810bb46db77e5c6194e797cebd430a8
Deepak <113835801+Deepakkumar95@users.noreply.github.com>
2024-12-22 20:30:40 +0000

    completed data_ingestion and data_transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,121 @@
+import os
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path= os.path.join('artifacts','preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config= DataTransformationConfig()
+
+    
+    def get_data_transformation_object(self):
+        try:
+            logging.info("data tranformation initiated")
+
+            #Define which column should by ordinal-encoded and which should be scaled
+            categorical_cols= ["cut", "color", "clarity"]
+            numerical_cols= ["carat", "depth", "table", "x", "y", "z"]
+
+            #Define the custom ranking for each ordinal variable
+            cut_categories= ["Fair", "Good", "Very Good", "Premium", "Ideal"]
+            color_categories= ["D", "E", "F", "E", "I", "J"]
+            clarity_categories= ["I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"]
+
+            logging.info("pipeline initiated")
+
+            num_pipeline= Pipeline(
+                steps= [
+                    ("Imputer",SimpleImputer(strategy="median")),
+                    ("Scaler",StandardScaler())
+
+                ]
+            )
+
+            cat_pipeline= Pipeline(
+                steps=[
+                    ("Imputer", SimpleImputer(strategy="most_frequent")),
+                    ("ordinalencoder", OrdinalEncoder(categories=["cut_categories", "color_categories", "clarity_categories"])),
+                    ("scaler", StandardScaler())
+                ]
+                )
+            
+            preprocessor= ColumnTransformer(transformers=
+                [
+                    ("num_pipeline", num_pipeline, numerical_cols),
+                    ("cat_pipeline", cat_pipeline, categorical_cols)
+                ]
+            )
+
+            return preprocessor
+
+            logging.info("pipeline completed")
+
+        except Exception as e:
+            logging.info("error occured in Data Transformation")
+            raise CustomException(e,sys)
+        
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            #Reading train and test data
+            train_df= pd.read_csv(train_path)
+            test_df= pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+            logging.info(f"Train DataFrame Head: \n {train_df.head().to_string()}")
+            logging.info(f"Test DataFrame Head: \n {test_df.head().to_string()}")
+
+            logging.info("obtaining preprocessing object")
+
+            preprocessor_obj= self.get_data_transformation_object()
+
+            target_column_name= "price"
+            drop_columns=[target_column_name, "id"]
+
+            input_feature_train_df= train_df.drop(columns=drop_columns, axis=1)
+            target_feature_train_df= train_df[target_column_name]
+
+            input_feature_test_df= test_df.drop(columns=drop_columns, axis=1)
+            target_feature_test_df= test_df[target_column_name]
+
+            #Transformation using preprocessor object
+            input_feature_train_arr= preprocessor_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr= preprocessor_obj.transform(input_feature_test_df)
+            
+            logging.info("Applying proprocessing object on training and test dataset")
+
+            train_arr= np.c_(input_feature_train_arr,np.array(target_feature_train_df))
+            test_arr= np.c_(input_feature_test_arr, np.array(target_feature_test_df))
+
+            save_object(file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                        obj=preprocessor_obj)
+            
+            logging.info("preprocessor pickle file saved")
+            
+
+            return(train_arr, 
+                   test_arr,
+                   self.data_transformation_config.preprocessor_obj_file_path
+                   )
+
+            
+
+        except Exception as e:
+            logging.info("error occured at initiate_data_transformation")
+            raise CustomException(e,sys)
+
